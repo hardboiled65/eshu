42c1188b1fa74510da559b9dc50e4443edb2f9e3
Yujeonja <hardboiled65@gmail.com>
2021-09-27 16:13:38 +0900

    Locale tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
+pub mod locale;
+
 #[cfg(test)]
 mod tests {
+    use super::locale::Locale;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn compare_locales() {
+        let locale = Locale::JaJp;
+        assert_eq!(locale, Locale::JaJp);
+    }
+
+    #[test]
+    fn locale_new() {
+        // let locale1 = Locale::new("en_US");
+        let locale2 = Locale::new("no_NO");
+        match locale2 {
+            Ok(locale) => { println!("{:?}", locale); },
+            Err(err) => {
+                panic!("{}", err);
+            },
+        }
     }
 }
---
+++ src/locale.rs
@@ -0,0 +1,52 @@
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug, PartialEq, Eq)]
+#[repr(u16)]
+pub enum Locale {
+    EnUs,
+    EnUk,
+    JaJp,
+    KoKr,
+}
+
+#[derive(Debug)]
+pub struct NoLocaleError {
+    locale: String,
+}
+
+impl Error for NoLocaleError {
+    fn description(&self) -> &str {
+        "No such locale."
+    }
+}
+
+impl fmt::Display for NoLocaleError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "No such locale: {}", self.locale)
+    }
+}
+
+impl Locale {
+    pub fn new(string: &str) -> Result<Locale, NoLocaleError> {
+        match string {
+            "en_US" => {
+                Ok(Locale::EnUs)
+            },
+            "en_UK" => {
+                Ok(Locale::EnUk)
+            },
+            "ja_JP" => {
+                Ok(Locale::JaJp)
+            },
+            "ko_KR" => {
+                Ok(Locale::KoKr)
+            },
+            _ => {
+                Err(NoLocaleError {
+                    locale: string.to_string(),
+                })
+            }
+        }
+    }
+}
