e90645f42da60fd0ab66a7745a4da4ce99933892
Yujeonja <hardboiled65@gmail.com>
2021-09-29 13:20:01 +0900

    Add templates directory to cldr-tool.

--- tools/cldr-tool/cldr-tool.py
+++ tools/cldr-tool/cldr-tool.py
@@ -3,7 +3,12 @@ import os
 
 import data_utils
 
+TEMPLATES_DIR = os.path.realpath(os.path.join(
+    data_utils.BASEDIR, '../../templates'))
+
 if __name__ == '__main__':
     if not data_utils.has_data():
         data_utils.download_data()
         data_utils.unzip_data()
+
+    print(TEMPLATES_DIR)
